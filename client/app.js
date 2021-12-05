f8ee95f064ed2bd42a578d867082e8b5e29806e3
Filip Frysz <ffrysz@gmail.com>
2021-12-05 22:04:34 +0100

    Add app code

---
+++ client/app.js
@@ -0,0 +1,38 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName = '';
+
+loginForm.addEventListener('submit', (event) => login(event));
+addMessageForm.addEventListener('submit', (event) => sendMessage(event));
+
+function login(event) {
+  event.preventDefault();
+  if (userNameInput.value) {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  } else alert('Input your name first!');
+}
+
+function sendMessage(e) {
+  e.preventDefault();
+  if (messageContentInput) {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  } else alert('Please type your message first');
+}
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message', 'message--received');
+  if (author === userName) message.classList.add('message--self');
+  const header = `<h3 class="message__author">${author === userName ? 'You' : author}</h3>`;
+  const text = `<div class="message__content">${content}</div>`;
+  message.innerHTML = header + text;
+  console.log(message);
+}
